e1d63db4f151b795ec1024e66bea12aa4af556ea
Barbara Verhaar <barbaraverhaar@MacBookBarbara.local>
2023-11-29 08:43:21 +0100

    ml script adj for kreat

---
+++ scripts/machinelearning_urine_kreat.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -c 24
+#SBATCH --mem=16G
+#SBATCH --time=12:00:00
+#SBATCH -p 'genoa'
+eval "$(conda shell.bash hook)"
+conda activate xgb
+python /projects/0/prjs0784/ckd-metabolomics/scripts/xgboost/XGBeast.py \
+    -name nonD_CKD_urinekreat \
+    -path /projects/0/prjs0784/ckd-metabolomics/CKD_urine_kreat \
+    -x class \
+    -n 200 \
+    -t 24 \
+    -rand_seed 1234 \
+    -param /projects/0/prjs0784/ckd-metabolomics/scripts/xgboost/param_medium.json
+python /projects/0/prjs0784/ckd-metabolomics/scripts/xgboost/XGBeast.py \
+    -name nonD_CKD_urinekreat \
+    -path /projects/0/prjs0784/ckd-metabolomics/CKD_urine_kreat \
+    -x class \
+    -n 200 \
+    -rand_seed 1234 \
+    -t 24 \
+    -permute \
+    -param /projects/0/prjs0784/ckd-metabolomics/scripts/xgboost/param_medium.json
